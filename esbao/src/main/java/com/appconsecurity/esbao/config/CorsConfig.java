2bbacae4f0c95aaf7f0e2370124d3ad7fa0562d5
wirisoft <wirideveloper@gmail.com>
2024-08-05 17:42:05 -0600

    unico consumo

--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100", "https://wirisoft-bcb34.web.app",  "http://200.58.106.203", "http://vps-4292454-x.dattaweb.com")
+                .allowedOrigins("http://vps-4292454-x.dattaweb.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true)
