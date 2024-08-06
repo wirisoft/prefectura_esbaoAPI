61524f301c95f34373452f410118a2698395c7da
wirisoft <wirideveloper@gmail.com>
2024-08-06 15:20:39 -0600

    agregado el localhost

--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -19,29 +19,24 @@ import java.io.IOException;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
-//    private static final String[] ALLOWED_ORIGINS = {
-//
-//    };
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://vps-4292454-x.dattaweb.com:8080",
-//                        "http://vps-4243804-x.dattaweb.com",
-//                        "http://localhost:8100")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-//                .allowedHeaders("*")
-//                .exposedHeaders("Authorization")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean<CustomCorsFilter> corsFilter() {
-//        FilterRegistrationBean<CustomCorsFilter> registrationBean = new FilterRegistrationBean<>();
-//        registrationBean.setFilter(new CustomCorsFilter(ALLOWED_ORIGINS));
-//        registrationBean.addUrlPatterns("/*");
-//        return registrationBean;
-//    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8100", "https://wirisoft-bcb34.web.app",  "http://200.58.106.203", "http://vps-4292454-x.dattaweb.com", "http://vps-4292454-x.dattaweb.com:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+
+        registry.addMapping("/auth/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "POST")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
 
 }
--- esbao/src/main/java/com/appconsecurity/esbao/config/CustomCorsFilter.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CustomCorsFilter.java
@@ -1,42 +1,42 @@
-//package com.appconsecurity.esbao.config;
-//
-//import jakarta.servlet.*;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class CustomCorsFilter extends OncePerRequestFilter {
-//
-//    private final List<String> allowedOrigins;
-//
-//    public CustomCorsFilter(String[] allowedOrigins) {
-//        this.allowedOrigins = Arrays.asList(allowedOrigins);
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//
-//        String origin = request.getHeader("Origin");
-//
-//        if (allowedOrigins.contains(origin)) {
-//            response.setHeader("Access-Control-Allow-Origin", "*");
-//            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-//            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-//            response.setHeader("Access-Control-Expose-Headers", "xsrf-token");
-//            response.setHeader("Access-Control-Allow-Credentials", "true");
-//            response.setHeader("Access-Control-Max-Age", "3600");
-//        }
-//
-//        if ("OPTIONS".equals(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//        } else {
-//            filterChain.doFilter(request, response);
-//        }
-//    }
-//
-//}
+package com.appconsecurity.esbao.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomCorsFilter extends OncePerRequestFilter {
+
+    private final List<String> allowedOrigins;
+
+    public CustomCorsFilter(String[] allowedOrigins) {
+        this.allowedOrigins = Arrays.asList(allowedOrigins);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String origin = request.getHeader("Origin");
+
+        if (allowedOrigins.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+            response.setHeader("Access-Control-Expose-Headers", "xsrf-token");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Max-Age", "3600");
+        }
+
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+}
