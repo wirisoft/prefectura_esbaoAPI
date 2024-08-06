2c155e2ede6b62496527e36bcfbab29b813300fa
wirisoft <wirideveloper@gmail.com>
2024-08-06 04:07:00 -0600

    cors globales1

--- esbao/src/main/java/com/appconsecurity/esbao/EsbaoApplication.java
+++ esbao/src/main/java/com/appconsecurity/esbao/EsbaoApplication.java
@@ -37,7 +37,7 @@ public class EsbaoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://vps-4292454-x.dattaweb.com")
+						.allowedOrigins("http://vps-4292454-x.dattaweb.com:8080")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -17,12 +17,14 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://vps-4292454-x.dattaweb.com")
+                        .allowedOrigins("http://vps-4292454-x.dattaweb.com:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
+
+
 
 }
