16dbcde85524256c443c20bfcfef1a8cb67a5e83
wirisoft <wirideveloper@gmail.com>
2024-08-06 05:07:44 -0600

    cors configuration1

--- esbao/src/main/java/com/appconsecurity/esbao/controllers/UserControllers.java
+++ esbao/src/main/java/com/appconsecurity/esbao/controllers/UserControllers.java
@@ -31,7 +31,7 @@ public class UserControllers {
         }
     }
 
-    //@CrossOrigin(origins = "http://vps-4243804-x.dattaweb.com:8080/users")
+    @CrossOrigin(origins = "http://vps-4243804-x.dattaweb.com:8080")
     @GetMapping("/get-all")
     public ResponseEntity<List<UserEntity>> getAllUsers() {
         try {
@@ -42,7 +42,7 @@ public class UserControllers {
         }
     }
 
-    //@CrossOrigin(origins = "http://vps-4243804-x.dattaweb.com:8080/users")
+    @CrossOrigin(origins = "http://vps-4243804-x.dattaweb.com:8080")
     @GetMapping("/get/{id}")
     public ResponseEntity<UserEntity> getUserById(@PathVariable Long id) {
         try {
