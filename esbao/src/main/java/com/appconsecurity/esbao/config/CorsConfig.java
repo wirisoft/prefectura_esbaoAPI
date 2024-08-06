d49d41143c373858079f372eb81cfe76a2f7d1e9
wirisoft <wirideveloper@gmail.com>
2024-08-06 04:54:36 -0600

    cors globales1

--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://vps-4292454-x.dattaweb.com:8080", "http://vps-4292454-x.dattaweb.com")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
