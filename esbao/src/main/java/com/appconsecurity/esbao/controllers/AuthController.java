297ce1c8d4e0898d96b333d6003a5c3643993d88
wirisoft <wirideveloper@gmail.com>
2024-08-05 19:20:34 -0600

    crossorigin

--- esbao/src/main/java/com/appconsecurity/esbao/controllers/AuthController.java
+++ esbao/src/main/java/com/appconsecurity/esbao/controllers/AuthController.java
@@ -8,16 +8,15 @@ import com.appconsecurity.esbao.services.models.dtos.ResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Optional;
 
+
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*") // Permite todas las solicitudes de cualquier origen
 public class AuthController {
 
     private final IAuthService authService;
