7ee7c58ef13c9b90ecaef9d83a972d5d60c640e1
wirisoft <wirideveloper@gmail.com>
2024-08-06 14:52:38 -0600

    agregado el localhost

--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -20,15 +20,15 @@ import java.io.IOException;
 public class CorsConfig implements WebMvcConfigurer {
 
     private static final String[] ALLOWED_ORIGINS = {
-            "http://vps-4292454-x.dattaweb.com:8080",
-            "http://vps-4243804-x.dattaweb.com",
-            "http://localhost:8100"
+
     };
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(ALLOWED_ORIGINS)
+                .allowedOrigins("http://vps-4292454-x.dattaweb.com:8080",
+                        "http://vps-4243804-x.dattaweb.com",
+                        "http://localhost:8100")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
