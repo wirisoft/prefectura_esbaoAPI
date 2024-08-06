20883ecc890a5082141019c35340d87d7f824ec3
wirisoft <wirideveloper@gmail.com>
2024-08-05 19:26:04 -0600

    crossorigin

--- esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
+++ esbao/src/main/java/com/appconsecurity/esbao/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.appconsecurity.esbao.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,19 +9,17 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
+        // Configuración global para todos los endpoints
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100", "https://wirisoft-bcb34.web.app",  "http://200.58.106.203", "http://vps-4292454-x.dattaweb.com")
+                .allowedOrigins(
+                        "http://localhost:8100",
+                        "https://wirisoft-bcb34.web.app",
+                        "http://200.58.106.203",
+                        "http://vps-4292454-x.dattaweb.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true)
-                .maxAge(3600);
-
-
-        registry.addMapping("/auth/**")
-                .allowedOrigins("*")
-                .allowedMethods("OPTIONS", "POST")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(false)
                 .maxAge(3600);
     }
 
