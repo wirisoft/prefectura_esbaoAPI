1cf5c3026a2c30e7217b268b5863347621b8325e
wirisoft <wirideveloper@gmail.com>
2024-08-06 16:29:54 -0600

    cors

--- esbao/src/main/java/com/appconsecurity/esbao/EsbaoApplication.java
+++ esbao/src/main/java/com/appconsecurity/esbao/EsbaoApplication.java
@@ -32,21 +32,25 @@ public class EsbaoApplication {
 	}
 
 	@Bean
-	public void addCorsMappings(CorsRegistry registry){
-		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:8100", "https://wirisoft-bcb34.web.app",  "http://200.58.106.203", "http://vps-4292454-x.dattaweb.com", "http://vps-4292454-x.dattaweb.com:8080")
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-				.allowCredentials(true)
-				.maxAge(3600);
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8100", "https://wirisoft-bcb34.web.app", "http://200.58.106.203", "http://vps-4292454-x.dattaweb.com", "http://vps-4292454-x.dattaweb.com:8080")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+						.allowCredentials(true)
+						.maxAge(3600);
 
-
-		registry.addMapping("/auth/**")
-				.allowedOrigins("*")
-				.allowedMethods("OPTIONS", "POST")
-				.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-				.allowCredentials(false)
-				.maxAge(3600);
+				registry.addMapping("/auth/**")
+						.allowedOrigins("*")
+						.allowedMethods("OPTIONS", "POST")
+						.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+						.allowCredentials(false)
+						.maxAge(3600);
+			}
+		};
 	}
 
 }
